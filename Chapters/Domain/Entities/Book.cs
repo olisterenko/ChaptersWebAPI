8642a67c49a2eb6957a9b0b924103564c5b13a83
olisterenko <o.listerenko@gmail.com>
2024-03-09 14:04:20 +0300

    revert: drop column

--- Chapters/Domain/Entities/Book.cs
+++ Chapters/Domain/Entities/Book.cs
@@ -5,6 +5,7 @@ public class Book : BaseEntity<int>
     public string Title { get; set; } = default!;
     public string Author { get; set; } = default!;
 
+    public double Rating { get; set; }
     public int YearWritten { get; set; }
     public string? Cover { get; set; }
 
