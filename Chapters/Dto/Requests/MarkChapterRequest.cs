3eaaf576b62a4ed4169c209ad417aab979aa528a
olisterenko <o.listerenko@gmail.com>
2024-03-09 17:36:49 +0300

    refactor

--- Chapters/Dto/Requests/MarkChapterRequest.cs
+++ Chapters/Dto/Requests/MarkChapterRequest.cs
@@ -1,6 +1,7 @@
 ﻿namespace Chapters.Dto.Requests;
 
-public sealed class MarkChapterRequest{
+public sealed class MarkChapterRequest
+{
     public required string Username { get; set; }
     public int ChapterId { get; set; }
 }
--- Chapters/Dto/Requests/UnmarkChapterRequest.cs
+++ Chapters/Dto/Requests/UnmarkChapterRequest.cs
@@ -1,6 +1,7 @@
 ﻿namespace Chapters.Dto.Requests;
 
-public sealed class UnmarkChapterRequest{
+public sealed class UnmarkChapterRequest
+{
     public required string Username { get; set; }
     public int ChapterId { get; set; }
 }
