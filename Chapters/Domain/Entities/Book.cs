078de5555400cba13b6c8b4542e317ded8c24def
olisterenko <o.listerenko@gmail.com>
2024-03-09 13:57:11 +0300

    feature: drop rating column in book table

--- Chapters/Domain/Entities/Book.cs
+++ Chapters/Domain/Entities/Book.cs
@@ -4,13 +4,11 @@ public class Book : BaseEntity<int>
 {
     public string Title { get; set; } = default!;
     public string Author { get; set; } = default!;
-    
-    // TODO: выпилить рейтинг
-    public double Rating { get; set; }
-    public int YearWritten { get; set; }
-    public string? Cover { get; set; } = default;
 
-    public List<UserBook> UserBooks { get; set; } = new();
-    public List<Review> Reviews { get; set; } = new();
-    public List<Chapter> Chapters { get; set; } = new();
+    public int YearWritten { get; set; }
+    public string? Cover { get; set; }
+
+    public List<UserBook> UserBooks { get; set; } = [];
+    public List<Review> Reviews { get; set; } = [];
+    public List<Chapter> Chapters { get; set; } = [];
 }
---
+++ Chapters/Migrations/202403091335_DropRatingColumn.cs
@@ -0,0 +1,11 @@
+﻿using FluentMigrator;
+
+namespace Chapters;
+
+[TimestampedMigration(2024, 3, 9, 13, 34)]
+public class DropRatingColumn : ForwardOnlyMigration {
+    public override void Up()
+    {
+        Delete.Column("Rating").FromTable("Book");
+    }
+}
