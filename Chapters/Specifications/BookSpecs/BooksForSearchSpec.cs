b50eaf207d230e9b8f27675ed2bdf7d38c6d42eb
olisterenko <o.listerenko@gmail.com>
2024-03-24 14:59:54 +0300

    fix: search contains culture

--- Chapters/Specifications/BookSpecs/BooksForSearchSpec.cs
+++ Chapters/Specifications/BookSpecs/BooksForSearchSpec.cs
@@ -8,7 +8,7 @@ public sealed class BooksForSearchSpec : Specification<Book>
     public BooksForSearchSpec(string q)
     {
         Query
-            .Where(b => b.Title.Contains(q, StringComparison.CurrentCultureIgnoreCase))
+            .Where(b => b.Title.Contains(q, StringComparison.InvariantCultureIgnoreCase))
             .OrderByDescending(x => x.Rating)
             .Take(500)
             .Include(x => x.UserBooks).ThenInclude(ub => ub.User);
